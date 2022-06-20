d81d07c56649c0c0fa502d1da0d987f877d5018a
Igor <igorpasqualino@gmail.com>
2022-06-20 16:34:40 -0300

    New Update Mode Games and Start the game

--- Js/QuizScript.js
+++ Js/QuizScript.js
@@ -9,12 +9,31 @@ let _result = document.getElementById('Results');
 let _Points = document.getElementById('PointsShow')
 let _checkBtn = document.getElementById('BtnCheck');
 let _retryBtn = document.getElementById('Btnretry');
-let _selectedAnswer = document.getElementsByClassName('selected')
-let correctAnswer = "", correctScore = askedCount = 0, totalQuestion = 10, QuestionCorrect = 0, difficultOfQuestion = "", PointScores= 0;
-
+let _selectedAnswer = document.getElementsByClassName('selected');
+let _QuizTable = document.querySelector('.QuizTable');
+let _BtnStart = document.getElementById('Start');
+let _RuleTable = document.querySelector('.QuizStart');
+let _BackToMenu = document.getElementById('BtnBackToMenu')
+let _RdbShort = document.getElementById('Short');
+let _RdbModerate = document.getElementById('Moderate');
+let _RdbLong = document.getElementById('Long');
+let _RdbMixed = document.getElementById('Mixed');
+let _RdbEasy = document.getElementById('Easy');
+let _RdbMedium = document.getElementById('Medium');
+let _RdbHard = document.getElementById('Hard');
+let correctAnswer = "", correctScore = askedCount = 0, totalQuestion = 10, QuestionCorrect = 0, difficultOfQuestion = "", PointScores = 0, ScoreStrick = 0;
 
 async function SendApiRequest() {
-    let response = await fetch('https://opentdb.com/api.php?amount=1');
+    let response;
+    if(_RdbMixed.checked == true){
+     response = await fetch('https://opentdb.com/api.php?amount=1');
+    } else if(_RdbEasy.checked == true){
+      response = await fetch('https://opentdb.com/api.php?amount=1&difficulty=easy');
+    } else if(_RdbMedium.checked == true){
+      response = await fetch('https://opentdb.com/api.php?amount=1&difficulty=medium');
+    } else{
+      response = await fetch('https://opentdb.com/api.php?amount=1&difficulty=hard');
+    }
     let data = await response.json();
     _result.innerHTML = "";
     useApiData(data.results[0]);
@@ -22,22 +41,36 @@ async function SendApiRequest() {
 function eventListeners() {
     _checkBtn.addEventListener('click', checkAnswer);
     _retryBtn.addEventListener('click', retryQuiz);
+    _BtnStart.addEventListener('click', StartTheGame);
+    _BackToMenu.addEventListener('click', BackToMainMenu);
 }
-
 document.addEventListener('DOMContentLoaded', function () {
-    SendApiRequest();
     eventListeners()
-    _totalQuestion.textContent = totalQuestion;
     _correctScore.textContent = correctScore;
-
 })
+function StartTheGame() {
+    if(_RdbShort.checked == true){
+        totalQuestion = 10;
+        _totalQuestion.textContent = 10;
+    } else if(_RdbModerate.checked == true){
+        totalQuestion = 20;
+        _totalQuestion.textContent = 20;
+    }else{
+        totalQuestion = 30
+        _totalQuestion.textContent = 30;
+    }
+    SendApiRequest();
+    setTimeout(function () {
+        _RuleTable.style.display = "none";
+        _QuizTable.style.display = "block";
+    }, 500);
+}
 function useApiData(data) {
     _checkBtn.disabled = false;
     correctAnswer = data.correct_answer;
     difficultOfQuestion = data.difficulty;
     let incorrectAnswer = data.incorrect_answers;
     let optionsList = incorrectAnswer;
-
     optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer);
     _question.innerHTML = `${data.question}`
     _difficulty.innerHTML = `<p>Difficulty: ${data.difficulty}</p>`
@@ -53,35 +86,31 @@ function selectOption() {
                 const activeOption = _options.querySelector('#selected');
                 activeOption.removeAttribute('id');
             }
-            option.setAttribute('id','selected');
-
+            option.setAttribute('id', 'selected');
         });
     });
 }
 
 function checkAnswer() {
     _checkBtn.disabled = true;
-   btnColorEnable();
+    btnColorEnable();
     if (_options.querySelector('#selected')) {
         let selectedAnswer = _options.querySelector('#selected span').textContent;
         if (selectedAnswer == HTMLDecode(correctAnswer)) {
             QuestionCorrect++;
             correctScore++;
-            if(difficultOfQuestion == "hard"){
-                PointScores += 100;
-            } else if(difficultOfQuestion == "medium") {
-                PointScores += 50;
-            } else {
-                PointScores += 25;
-            }
+            ScoreStrickCount();
+            PointCalculate();
             _result.innerHTML = `<p class="ResultsTextCorrect"> <i class = "fas fa-check"></i>correct answer! </p>`;
         } else {
+            _QuizTable.classList.remove('Onfire');
             _result.innerHTML = `<p class="ResultsTextIncorrect"> <i class = "fas fa-check"></i> incorrect answer!<small><b> Correct Answer: </b> ${correctAnswer}</small> </p>`;
             correctScore++;
+            ScoreStrick = 0;
         }
         checkCount();
     } else {
-           btnColorDisable();
+        btnColorDisable();
         _result.innerHTML = `<p class="SelectionOptionAtencion"><i class = "fas fa-question"></i>Please select an option!</p>`;
         _checkBtn.disabled = false;
     }
@@ -99,6 +128,7 @@ function checkCount() {
         }, 5000);
         _Points.innerHTML = `<div class="ScorePoints"><p class="QuestionCorrect">Your correct answers was ${QuestionCorrect}.</p></div> <div class="ScorePoints"> <p class="QuestionCorrect">Your point is ${PointScores}</p></div>`;
         _retryBtn.style.display = "block";
+        _BackToMenu.style.display = "block";
         _checkBtn.style.display = "none";
     } else {
         setTimeout(function () {
@@ -114,21 +144,48 @@ function setCount() {
 }
 
 function retryQuiz() {
-    correctScore = askedCount = 0;
-    _correctScore.innerHTML = 0;
-    PointScores = 0;
-    QuestionCorrect = 0;
+    Reset();
     btnColorDisable();
-    _retryBtn.style.display = "none";
-    _checkBtn.style.display = "block";
-    _checkBtn.disabled = false;
-    SendApiRequest()
+    SendApiRequest();
 }
-function btnColorEnable(){
+function PointCalculate() {
+    if (difficultOfQuestion == "hard") {
+        PointScores += 100;
+    } else if (difficultOfQuestion == "medium") {
+        PointScores += 50;
+    } else {
+        PointScores += 25;
+    }
+}
+function ScoreStrickCount() {
+    ScoreStrick++;
+    if (ScoreStrick > 3) {
+        _QuizTable.classList.add('Onfire');
+        PointScores += 20;
+    }
+}
+function BackToMainMenu() {
+    Reset();
+    _RuleTable.style.display = "block";
+    _QuizTable.style.display = "none";
+}
+function btnColorEnable() {
     _checkBtn.style.background = "#660066";
     _checkBtn.style.color = "#ffffff";
 }
-function btnColorDisable(){
-    _checkBtn.style.background = "";
-    _checkBtn.style.color = "";
+function btnColorDisable() {
+    _checkBtn.style.background = "#eabfff";
+    _checkBtn.style.color = "#000000";
+}
+function Reset() {
+    correctScore = askedCount = 0;
+    _correctScore.innerHTML = 0;
+    _Points.innerHTML = "";
+    PointScores = 0;
+    QuestionCorrect = 0;
+    ScoreStrick = 0;
+    _retryBtn.style.display = "none";
+    _BackToMenu.style.display = "none";
+    _checkBtn.style.display = "block";
+    _checkBtn.disabled = false;
 }
